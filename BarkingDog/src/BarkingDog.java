b8e4230da98fde49e4f74a48479ebac43602b44f
silkyprogrammer <hpalappe@gmail.com>
2021-04-26 11:33:00 +0530

    All the work so far - 26/Apr/2021.

--- BarkingDog/src/BarkingDog.java
+++ BarkingDog/src/BarkingDog.java
@@ -1,2 +1,15 @@
 public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+        if ( hourOfDay<0 || hourOfDay>23){
+            return false;
+        }
+        else if (barking && hourOfDay<8){
+            return true;
+        }
+        else if (barking && hourOfDay > 22 ){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
--- BarkingDog/src/Main.java
+++ BarkingDog/src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+//        System.out.println(BarkingDog.shouldWakeUp(true,-1));
+        System.out.println(BarkingDog.shouldWakeUp(true,22));
+    }
 }
