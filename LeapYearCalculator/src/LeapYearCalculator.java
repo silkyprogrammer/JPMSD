47002b30eaadac3723a626041c39f8586e582ae7
silkyprogrammer <hpalappe@gmail.com>
2021-04-27 09:46:05 +0530

    All the work so far - 27/Apr/2021.

--- LeapYearCalculator/src/LeapYearCalculator.java
+++ LeapYearCalculator/src/LeapYearCalculator.java
@@ -1,2 +1,28 @@
 public class LeapYearCalculator {
+    public static boolean isLeapYear(int year){
+        boolean leap = false;
+        if (year<=1 || year>=9999){
+            return false;
+        }
+        // if the year is divided by 4
+        if (year % 4 == 0) {
+            // if the year is century
+            if (year % 100 == 0) {
+                // if year is divided by 400
+                // then it is a leap year
+                if (year % 400 == 0)
+                    leap = true;
+                else
+                    leap = false;
+            }
+            // if the year is not century
+            else
+                leap = true;
+        }
+        else{
+            leap = false;
+        }
+        return leap;
+
+    }
 }
