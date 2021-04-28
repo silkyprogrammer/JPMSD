aa7375dd2210f0adcc291230d1a61cabc7a9cd9b
silkyprogrammer <hpalappe@gmail.com>
2021-04-28 07:04:47 +0530

    All the work so far - 28/Apr/2021.

--- DecimalComparator/src/DecimalComparator.java
+++ DecimalComparator/src/DecimalComparator.java
@@ -1,2 +1,15 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double a, double b){
+        boolean result = false;
+        BigDecimal a1 = new BigDecimal(a*1000).setScale(0,BigDecimal.ROUND_DOWN);
+        System.out.println(a1);
+        BigDecimal b1 = new BigDecimal(b*1000).setScale(0,BigDecimal.ROUND_DOWN);
+        System.out.println(b1);
+        result = (a1.compareTo(b1)==0);
+
+        return result;
+    }
 }
--- DecimalComparator/src/Main.java
+++ DecimalComparator/src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+//        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(3.0,3.0));
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(-3.1756,-3.175));
+    }
 }
